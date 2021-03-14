160d620ce2432bd8a1764974cc1198b8fd6d4135
ivensgustavo <gustavo_ivens@alu.ufc.br>
2021-03-13 21:03:30 -0300

    Refatoracao metodo toPredicate em ComportamentoSpecification

--- src/main/java/br/ufc/quixada/npi/gestaocompetencia/model/ComportamentoSpecification.java
+++ src/main/java/br/ufc/quixada/npi/gestaocompetencia/model/ComportamentoSpecification.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
@@ -21,14 +22,84 @@ public class ComportamentoSpecification implements Specification<Comportamento>{
 	private static final String UNIDADE = "unidade";
 	private static final String COMPETENCIA = "competencia";
 	private ComportamentoSearch criteria;
+	private Root<Comportamento> root;
+	private CriteriaBuilder criteriaBuilder;
+	final List<Predicate> predicates = new ArrayList<>();
 	
 	public ComportamentoSpecification(ComportamentoSearch criteria) {
 		this.criteria = criteria;
 	}
-
+	
+	private boolean comparaSituacao(SituacaoComportamento tipoSituacao) {
+		return criteria.getSituacao().equals(tipoSituacao);
+	}
+	
+	private void addPredicadoQuandoSituacaoIgualANaoAssociados() {
+		predicates.add(criteriaBuilder.and(criarPredicadoIsNull(COMPETENCIA),criarPredicadoIsFalse(EXCLUIDO)));
+	}
+	
+	private void addPredicadoQuandoSituacaoIgualAAssociados() {
+		predicates.add(criteriaBuilder.and(criarPredicadoIsNotNull(COMPETENCIA),criarPredicadoIsFalse(EXCLUIDO)));
+	}
+	
+	private void addPredicadoQuandoSituacaoIgualANaoConsilidados() {
+		predicates.add(criteriaBuilder.and(criarPredicadoIsFalse("consolidado"), criarPredicadoIsFalse(EXCLUIDO)));
+	}
+	
+	private void addPredicadoQuandoSitucaoIgualAConsolidados() {
+		predicates.add(criarPredicadoIsTrue("consolidado"));
+	}
+	
+	private void addPredicadoQuandoSituacaoIgualAExcluidos() {
+		predicates.add(criarPredicadoIsTrue(EXCLUIDO));
+	}
+	
+	private void addPredicadoQuandoSituacaoIgualATodosOuOriginais() {
+		predicates.add(criarPredicadoIsFalse(EXCLUIDO));
+	}
+	
+	private void addPredicadoQuandoSituacaoIgualAEditados() {
+		predicates.add(
+				this.getPredicadoSitucaoIgualAEditados()
+			);
+	}
+	
+	private Predicate getPredicadoSitucaoIgualAEditados() {
+		return criteriaBuilder.and(
+				criarPredicadoIsNotNull("descricaoAtualizada"),
+				criarPredicadoIsFalse(EXCLUIDO),
+				criarPredicadoNotEqual("descricao", "descricaoAtualizada")
+			);
+	}
+	
+	private Predicate criarPredicadoIsNotNull(String campo) {
+		return criteriaBuilder.isNotNull(root.get(campo));
+	}
+	
+	private Predicate criarPredicadoIsNull(String campo) {
+		return criteriaBuilder.isNull(root.get(campo));
+	}
+	
+	private Predicate criarPredicadoIsFalse(String campo) {
+		return criteriaBuilder.isFalse(root.get(campo));
+	}
+	
+	private Predicate criarPredicadoIsTrue(String campo) {
+		return criteriaBuilder.isTrue(root.get(campo));
+	}
+	
+	private Predicate criarPredicadoNotEqual(String valor1, String valor2) {
+		return criteriaBuilder.notEqual(root.get(valor1), root.get(valor2));
+	}
+	
+	/*private void filtrarSituacaoEAddPredicados(Root<Comportamento> root, CriteriaBuilder criteriaBuilder) {
+		
+	}*/
 	@Override
 	public Predicate toPredicate(Root<Comportamento> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {	
-		final List<Predicate> predicates = new ArrayList<>();
+		
+		this.root = root;
+		this.criteriaBuilder = criteriaBuilder;
 
 		Path<Unidade> unidade = root.get(UNIDADE);
 		
@@ -42,26 +113,26 @@ public class ComportamentoSpecification implements Specification<Comportamento>{
 
 		// Filtro de Situação
 		if(criteria.getSituacao() != null) {
-			if(criteria.getSituacao().equals(SituacaoComportamento.NAO_ASSOCIADOS)) {
-				predicates.add(criteriaBuilder.and(criteriaBuilder.isNull(root.get(COMPETENCIA)),criteriaBuilder.isFalse(root.get(EXCLUIDO))));
-			} else if(criteria.getSituacao().equals(SituacaoComportamento.ASSOCIADOS)) {
-				predicates.add(criteriaBuilder.and(criteriaBuilder.isNotNull(root.get(COMPETENCIA)),criteriaBuilder.isFalse(root.get(EXCLUIDO))));
-			} else if(criteria.getSituacao().equals(SituacaoComportamento.NAO_CONSOLIDADOS)) {
-				predicates.add(criteriaBuilder.and(criteriaBuilder.isFalse(root.get("consolidado")),criteriaBuilder.isFalse(root.get(EXCLUIDO))));
-			}else if(criteria.getSituacao().equals(SituacaoComportamento.CONSOLIDADOS)) {
-				predicates.add(criteriaBuilder.isTrue(root.get("consolidado")));
-			} else if(criteria.getSituacao().equals(SituacaoComportamento.EXCLUIDOS)) {
-				predicates.add(criteriaBuilder.isTrue(root.get(EXCLUIDO)));
-			} else if(criteria.getSituacao().equals(SituacaoComportamento.TODOS) || criteria.getSituacao().equals(SituacaoComportamento.ORIGINAIS)) {
-				predicates.add(criteriaBuilder.isFalse(root.get(EXCLUIDO)));
-			} else if(criteria.getSituacao().equals(SituacaoComportamento.EDITADOS)) {
-				predicates.add(
-					criteriaBuilder.and(
-						criteriaBuilder.isNotNull(root.get("descricaoAtualizada")),
-						criteriaBuilder.isFalse(root.get(EXCLUIDO)),
-						criteriaBuilder.notEqual(root.get("descricao"), root.get("descricaoAtualizada"))
-					)
-				);
+			if(this.comparaSituacao(SituacaoComportamento.NAO_ASSOCIADOS)) {
+				this.addPredicadoQuandoSituacaoIgualANaoAssociados();
+				
+			} else if(this.comparaSituacao(SituacaoComportamento.ASSOCIADOS)) {
+				this.addPredicadoQuandoSituacaoIgualAAssociados();
+				
+			} else if(this.comparaSituacao(SituacaoComportamento.NAO_CONSOLIDADOS)) {
+				this.addPredicadoQuandoSituacaoIgualANaoConsilidados();
+				
+			}else if(this.comparaSituacao(SituacaoComportamento.CONSOLIDADOS)) {
+				this.addPredicadoQuandoSitucaoIgualAConsolidados();
+				
+			} else if(this.comparaSituacao(SituacaoComportamento.EXCLUIDOS)) {
+				this.addPredicadoQuandoSituacaoIgualAExcluidos();
+				
+			} else if(this.comparaSituacao(SituacaoComportamento.TODOS) || this.comparaSituacao(SituacaoComportamento.ORIGINAIS)) {
+				this.addPredicadoQuandoSituacaoIgualATodosOuOriginais();
+				
+			} else if(this.comparaSituacao(SituacaoComportamento.EDITADOS)) {
+				this.addPredicadoQuandoSituacaoIgualAEditados();
 			}
 		}
 
